92acb0a758798aa70503e366bf972940e54e270e
brada1604 <bagusnugroho1604@gmail.com>
2025-05-05 15:06:10 +0700

    clean code header

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -10,47 +10,53 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="clearlist navbar-nav ms-auto">
-                    		
-							<?php 
-                                $query = $this->db->get_where('tbl_navbar', array('navbar_parent_id' => 0));
-                                if($query->num_rows() > 0):
-                            ?>	
-                            <?php foreach ($query->result() as $row):?>
+                    <?php 
+                        $query = $this->db->get_where('tbl_navbar', array('navbar_parent_id' => 0));
+                        if($query->num_rows() > 0):
+                    ?>	
+
+                        <?php foreach ($query->result() as $row):?>
                             <?php 
-                                    $nav_id=$row->navbar_id;
-                                    $query2 = $this->db->get_where('tbl_navbar', array('navbar_parent_id' => $nav_id));
+                                $nav_id=$row->navbar_id;
+                                $query2 = $this->db->get_where('tbl_navbar', array('navbar_parent_id' => $nav_id));
                             ?>
+                        
                             <?php if($query2->num_rows() > 0):?>
+                            
                                 <li class="nav-item dropdown">
                                     <a href="<?php echo site_url($row->navbar_slug);?>" class="menu-down nav-link dropdown-toggle"><?php echo $row->navbar_name;?> </a>
-                
+
                                     <!-- Sub -->
                                     <ul class="nav-sub dropdown-menu rounded-0 m-0">
                                         <?php foreach ($query2->result() as $row2):?>
-                                        <li>
-                                            <a href="<?php echo site_url($row2->navbar_slug);?>" class="dropdown-item"><?php echo $row2->navbar_name;?></a>	
-                                        </li>
+                                            <li>
+                                                <a href="<?php echo site_url($row2->navbar_slug);?>" class="dropdown-item"><?php echo $row2->navbar_name;?></a>	
+                                            </li>
                                         <?php endforeach;?>
                                     </ul>
                                     <!-- End Sub -->
-                                    
+
                                 </li>
-                                <?php else:?>
+
+                            <?php else:?>
+
                                 <li>
                                     <a href="<?php echo site_url($row->navbar_slug);?>" class="nav-item nav-link"><?php echo $row->navbar_name;?></a>	
                                 </li>
-                                <?php endif;?>
-                            <?php endforeach;?>
-                            <?php else:?>
-                            <li>
-                                <a href="#">Belum ada menu</a>		
-                            </li>
                             <?php endif;?>
+
+                        <?php endforeach;?>
+
+                    <?php else:?>
+
+                        <li>
+                            <a href="#">Belum ada menu</a>		
+                        </li>
+
+                    <?php endif;?>
 
                 </ul>
             </div>
         </div>
     </nav>
 </div>
-                
-
